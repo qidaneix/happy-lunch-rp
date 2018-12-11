4cbf6e7a8934f8f3e1b1639fb02034dd1d059a3a
xiaodabao <xiaojin1992@126.com>
2018-12-12 01:16:27 +0800

    init success

--- main.py
+++ main.py
@@ -6,29 +6,28 @@ import schedule
 import time
 import cv2
 
-def job():
-    # take a photo
-    camera = cv2.VideoCapture(0)
-    return_value, image = camera.read()
-    if return_value:
+camera = cv2.VideoCapture(0)
+
+while (camera.isOpened()):
+    def job():
+        # take a photo
+        return_value, image = camera.read()
         # save the photo, use current time as name
         image_name = time.strftime('images/%Y-%m-%dT%H:%M:%S',time.localtime(time.time())) + '.jpg'
         cv2.imwrite(image_name, image)
         print '{} written!'.format(image_name)
-        camera.release()
-    else:
-        print return_value
 
-    # send a http request
-    # url = 'http://lunch.xiaodabao.xyz/image'
-    # files = {'file': open('test.jpg', 'rb')}
-    # r = requests.post(url, files = files)
+        # send a http request
+        url = 'http://lunch.xiaodabao.xyz/image'
+        files = {'file': open(image_name, 'rb')}
+        r = requests.post(url, files = files)
+        # handle http response
+        print r.content
 
-    # handle http response
-    # print r.content
+    schedule.every(5).seconds.do(job)
 
-schedule.every(5).seconds.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+camera.release()
