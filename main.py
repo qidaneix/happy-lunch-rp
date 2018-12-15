e2ed403bd9b1138774142af1f6a2d4f619306bf6
xiaodabao <xiaojin1992@126.com>
2018-12-15 10:19:20 -0800

    add rotate and delete

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import requests
 import schedule
 import time
 import cv2
+import numpy as np
 
 camera = cv2.VideoCapture(0)
 
@@ -14,19 +16,35 @@ while (camera.isOpened()):
         return_value, image = camera.read()
         # save the photo, use current time as name
         image_name = time.strftime('images/%Y-%m-%dT%H:%M:%S',time.localtime(time.time())) + '.jpg'
-        cv2.imwrite(image_name, image)
-        print 'image written!'
+
+        # rotate 180 degrees
+        # get image height, width
+        (h, w) = image.shape[:2
+                            ]
+        # calculate the center of the image
+        center = (w / 2, h / 2)
+        # Perform the counter clockwise rotation holding at the center
+        # 180 degrees
+        M = cv2.getRotationMatrix2D(center, 180, 1.0)
+        rotated180 = cv2.warpAffine(image, M, (w, h))
+
+        cv2.imwrite(image_name, rotated180)
+        print(image_name + 'image written!')
 
         # send a http request
         url = 'http://lunch.xiaodabao.xyz/image'
         files = {'file': open(image_name, 'rb')}
         r = requests.post(url, files = files)
         # handle http response
-        print r.content
+        print(r.content)
 
-    schedule.every(5).seconds.do(job)
+        # delete image
+        os.remove(image_name)
+        print(image_name + 'image deleted!')
 
-    while True:
+    schedule.every(10).seconds.do(job)
+
+    while (True):
         schedule.run_pending()
         time.sleep(1)
 
